1e5cfb41c2b0367f7bdfa5b82bf2d07dd4bfd8dc
gr1zzly-be4r <jake.bruemmer@gmail.com>
2015-05-30 12:41:41 -0500

    Added details of program output and how to run program to README.

--- README.md
+++ README.md
@@ -5,4 +5,12 @@ Instructions for playing the game can be found [here](http://www.drinkinggames.c
 
 Run the game by cloning the repo and running
 
-`python fuck_the_dealer.py`
+`python main.py num_players num_rounds [--verbose]`
+
+Specifying the `--verbose` option will result in a print out of a summary for each round in every game that you simulate. It will print out a lot. You can run the game over as many different values for the number of players and number of rounds in the allowable ranges, and you should see that the "smart" player will have the least amount of total drinks over all of the rounds.
+
+You can verify this by looking at the final print out of the program. `main.py` will print out a list showing the amount of drinks that each player has drunk over all of the games that you've simulated. So an output like:
+
+`[100, 200, 200]`
+
+would mean that the 0th player -- the smart player -- would have drank 100 oz. of whatever drink these imaginary players are playing with over all of the games. 
--- deck.py
+++ deck.py
@@ -6,16 +6,13 @@ class Deck:
     # This is the "base" deck of cards. Each of the cards has their face value followed by a single letter which denotes their suit.
 
     def __init__(self):
-        self.deck = []
         UNSHUFFLED_DECK = [
             'as', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'js', 'qs', 'ks',
             'ac', '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'jc', 'qc', 'kc',
             'ah', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', 'jh', 'qh', 'kh',
             'ad', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'jd', 'qd', 'kd']
 
-        for card in range(52):
-            # Crude way of shuffling the deck. The pop function will remove a random card from the deck
-            # and then that card will be appended to the deck of this Deck class.
-            self.deck.append(UNSHUFFLED_DECK.pop(random.randrange(len(UNSHUFFLED_DECK))))
+        random.shuffle(UNSHUFFLED_DECK)
+        self.deck = UNSHUFFLED_DECK
         self.played = []
 
